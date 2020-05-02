ab53bbdbdfa2df0d5d3818c7c342294d5b0cb9ac
Revati Tekale <revitekale1910@gmail.com>
2020-05-02 18:01:57 +0530

    ADDED:Service class to simply return Hello World

---
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.greetingapp.service;
+
+import com.bridgelabz.greetingapp.dto.Greeting;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public Greeting getMessage(String name) {
+        return new Greeting(name);
+    }
+
+    public Greeting getGreetingByParameter(String firstName, String lastName) {
+        return new Greeting(firstName + " " + lastName);
+    }
+
+    public String sayHello() {
+        return "Hello World";
+    }
+}
