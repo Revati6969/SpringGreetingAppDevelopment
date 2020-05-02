a23c2bdb991ddc5527209c27c6e02762f16dfb8c
Revati Tekale <revitekale1910@gmail.com>
2020-05-02 18:00:38 +0530

    ADDED:Extended controller class to use service layer

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -2,23 +2,28 @@ package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.dto.Greeting;
 import com.bridgelabz.greetingapp.dto.User;
+import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    @Autowired
+    GreetingService greetingService;
+
+    @RequestMapping(value = {"/", "", "/home"})
+    public String sayHello() {
+        return greetingService.sayHello();
+    }
+
     @GetMapping("/getGreeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(name);
+        return greetingService.getMessage(name);
     }
 
-    @RequestMapping(value = "/query", method = RequestMethod.GET)
-    public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(name);
-    }
-
-    @RequestMapping("/post")
-    public Greeting postGreeting(@RequestBody User user) {
-        return new Greeting(user.getFirstName() + " " + user.getLastName());
+    @PutMapping("/putParam")
+    public Greeting putGreeting(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
+        return greetingService.getGreetingByParameter(firstName, lastName);
     }
 }
