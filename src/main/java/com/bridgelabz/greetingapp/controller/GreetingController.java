1576c1f484c1756e214dc8d9fa1d69cebc91fa4f
Revati Tekale <revitekale1910@gmail.com>
2020-05-03 14:20:50 +0530

    MODIFIED:Save data in database

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.greetingapp.controller;
 
-import com.bridgelabz.greetingapp.dto.Greeting;
-import com.bridgelabz.greetingapp.dto.User;
+import com.bridgelabz.greetingapp.dto.UserDTO;
 import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +17,17 @@ public class GreetingController {
     }
 
     @GetMapping("/getGreeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+    public String greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
         return greetingService.getMessage(name);
     }
 
     @PutMapping("/putParam")
-    public Greeting putGreeting(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
+    public String putGreeting(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
         return greetingService.getGreetingByParameter(firstName, lastName);
+    }
+
+    @PostMapping("/create")
+    public UserDTO saveGreeting(@RequestBody UserDTO greetingDTO) {
+        return greetingService.saveGreeting(greetingDTO);
     }
 }
